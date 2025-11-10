d03bb92278eb8d0be7aaf905496f6f877e373e06
Navya Bajwa <navyabajwa25@gmail.com>
2025-11-10 10:48:45 -0500

    added more tests

--- tests/test_return_book_by_patron.py
+++ tests/test_return_book_by_patron.py
@@ -1,8 +1,18 @@
 import pytest
 
-import services.library_service as ls
+from services.library_service import (
+    borrow_book_by_patron, add_book_to_catalog, return_book_by_patron
+)
 import datetime
-import database as db
+from database import get_book_by_isbn
+
+from database import init_database, reset_database, add_sample_data
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    init_database()
+    reset_database()
+    add_sample_data()
 
 '''
 def test_return_book_valid(monkeypatch):
@@ -32,35 +42,57 @@ def test_return_book_valid(monkeypatch):
 
 def test_return_book_not_borrowed():
     """Test returning a book that the patron didn't withdraw."""
-    success, message = ls.return_book_by_patron("999999", 2)
-    
-    assert success == False
-    assert "book not found" in message.lower()
+    add_book_to_catalog("boooook", "meeeeee", "7642678944427", 2)
+    book = get_book_by_isbn("7642678944427")
+    bookID = book["id"]
+    borrow_book_by_patron("137865", bookID)
 
-def test_return_book_invalid_patron():
-    """Test returning a book with non-existant patron."""
-    success, message = ls.return_book_by_patron("000000", 3)
+    add_book_to_catalog("book2", "me3", "6642678944427", 2)
+    book2 = get_book_by_isbn("6642678944427")
+    bookIDnotBorrowed = book2["id"]
+    
+    success, message = return_book_by_patron("137865", bookIDnotBorrowed)
     
     assert success == False
-    assert "book not found" in message.lower()
+    assert "not currently borrowed" in message.lower()
+
+def test_return_book_successful():
+    add_book_to_catalog("book3", "author3", "5642648944427", 2)
+    book = get_book_by_isbn("5642648944427")
+    bookID = book["id"]
+    borrow_book_by_patron("133865", bookID)    
+
+    success, message = return_book_by_patron("133865", bookID)
+    assert success == True
+    assert "returned successfully" in message.lower()
+
 
 def test_return_book_invalid_book_id():
     """Test returning a book with non-existent book id."""
-    success, message = ls.return_book_by_patron("345678", 999)
+    success, message = return_book_by_patron("345678", 999)
     
     assert success == False
     assert "book not found" in message.lower()
 
 def test_return_book_no_patron_id():
     """Test returning a book with no patron id."""
-    success, message = ls.return_book_by_patron(" ", 4)
+    success, message = return_book_by_patron(" ", 4)
     
     assert success == False
     assert "invalid patron" in message.lower()
 
 def test_return_book_no_book_id():
     """Test returning a book with no book id."""
-    success, message = ls.return_book_by_patron("567890", None)
+    success, message = return_book_by_patron("567890", None)
     
     assert success == False
     assert "invalid book" in message.lower()
+
+'''
+def test_return_book_invalid_patron():
+    """Test returning a book with non-existant patron."""
+    success, message = ls.return_book_by_patron("000000", 3)
+    
+    assert success == False
+    assert "book not found" in message.lower()
+'''
