8ca0a64a10c1261e0dcc7f06c0515517f8253440
Navya Bajwa <navyabajwa25@gmail.com>
2025-11-10 14:14:55 -0500

    changed file name

--- tests/test_payment_mock_stub.py
+++ tests/test_payment_mock_stub.py
@@ -5,6 +5,7 @@ from unittest.mock import Mock
 import time
 
 
+#TESTS FOR PAYING LATE FEES
 def test_payLateFees_Successful(mocker):
     # test paying fees with valid input
     mock_calc_fee = mocker.patch(
@@ -136,9 +137,6 @@ def test_payLateFees_gatewayExceptionError(mocker):
         description=f"Late fees for 'Test Book'"
     )
 
-
-# Additional Tests
-
 def test_payLateFees_gatewayDenies_amountHigh(mocker):
     # test when the gateway declines the payment --> bc/ amount is too low
     mocker.patch(
@@ -204,3 +202,98 @@ def test_payLateFees_bookNotFound(mocker):
     assert transaction_id is None
 
     mock_payment_gateway.process_payment.assert_not_called()
+
+
+
+# Tests for Refunding Late Payment
+
+def test_refundPayment_Successful(mocker):
+    # test refunding a with valid inputs
+    stubTransactionID = f"txn_123456_{int(time.time())}"
+    stubAmount = 3.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    mock_payment_gateway.refund_payment.return_value = (True, f"Refund of ${stubAmount:.2f} processed successfully. Refund ID: {stubTransactionID}")
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == True
+    assert f"refund of ${stubAmount:.2f} processed successfully." in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_called_once()
+    mock_payment_gateway.refund_payment.assert_called_with(stubTransactionID, stubAmount)
+
+def test_refundPayment_invalidTransactionID(mocker):
+    # test refunding a with an invalid transaction id
+    stubTransactionID = f"weeeeeee_123456_{int(time.time())}"
+    stubAmount = 2.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == False
+    assert "invalid transaction id." in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_not_called()
+
+def test_refundPayment_amountTooLow(mocker):
+    # test refunding when the amount is <= 0
+    stubTransactionID = f"txn_567125_{int(time.time())}"
+    stubAmount = -2.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == False
+    assert "refund amount must be greater than 0." in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_not_called()
+
+def test_refundPayment_amountTooHigh(mocker):
+    # test refunding when the amound > 15
+    stubTransactionID = f"txn_567625_{int(time.time())}"
+    stubAmount = 20.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == False
+    assert "refund amount exceeds maximum late fee." in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_not_called()
+
+def test_refundPayment_networkException(mocker):
+    # test refunding when the amound > 15
+    stubTransactionID = f"txn_567445_{int(time.time())}"
+    stubAmount = 2.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    mock_payment_gateway.refund_payment.side_effect = Exception("network error")
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == False
+    assert "refund processing error" in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_called_once()
+    mock_payment_gateway.refund_payment.assert_called_with(stubTransactionID, stubAmount) 
+
+def test_refundPayment_refundFails(mocker):
+    # test refunding when the amound > 15
+    stubTransactionID = f"txn_227445_{int(time.time())}"
+    stubAmount = 5.00
+    mock_payment_gateway = Mock(spec=PaymentGateway)
+
+    mock_payment_gateway.refund_payment.return_value = (False, "invalid")
+    success, message = refund_late_fee_payment(stubTransactionID, stubAmount, mock_payment_gateway)
+
+    assert success == False
+    assert "refund failed" in message.lower()
+
+    mock_payment_gateway.refund_payment.assert_called_once()
+    mock_payment_gateway.refund_payment.assert_called_with(stubTransactionID, stubAmount)     
+
+
+
+
+
+
+    
